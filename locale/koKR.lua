dc4fad0fa02286c1346bde9a52ea7cf6112049b5
Christian Parpart <trapni@gentoo.org>
2011-06-29 14:06:24 +0200

    Added korean localization, thanks to 정영진 <kingeye2000@naver.com> and bump to 0.2.1.

---
+++ locale/koKR.lua
@@ -0,0 +1,43 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Atemi", "koKR") --sox2000
+
+if not L then return end
+
+L["Enable"] = "사용"
+L["Enables / disables this AddOn"] = "애드온 활성/비활성"
+
+L['General'] = "일반"
+L['General Settings'] = "일반 설정"
+L['Show greeter on load'] = '도움말 보기'
+L['Shows the addon greeting text in the main chat window when loading this addon.'] = "애드온 로딩시 기본채팅창에 도움말 표시."
+L['cooldown text color'] = "쿨다운 폰트 색상"
+L['The color the text should be drawn in'] = "폰트 색상을 변경하려면 클릭."
+L['Show spell tooltips'] = "주문 툴팁 보기"
+L['Shows tooltips of their spells when hovering the cooldown icon atop of a nameplate'] = ""
+L['Icon gap size'] = "아이콘 간격"
+L['gap in pixels between the cooldown icons'] = "아이콘 좌우 간격"
+L['Icon size'] = "아이콘 크기"
+L['size of the cooldown icons in pixels'] = ""
+L['font size'] = "폰트 크기"
+L['size of the cooldown text in pixels inside the cooldown icon'] = ""
+L['X-offset'] = "위치-가로"
+L['icon offset to the X-axis relative to its nameplate parent'] = "아이콘의 x 위치"
+L['Y-offset'] = "위치-세로"
+L['icon offset to the Y-axis relative to its nameplate parent'] = "아이콘의 y 위치"
+
+L['Cooldowns'] = "쿨다운"
+L['Rogue'] = "도적"
+L['Mage'] = "마법사"
+L['Priest'] = "사제"
+L['Hunter'] = "사냥꾼"
+L['Druid'] = "드루이드"
+L['Shaman'] = "주술사"
+L['Warlock'] = "흑마법사"
+L['Death Knight'] = "죽음의 기사"
+L['Warrior'] = "전사"
+L['Paladin'] = "성기사"
+L['Racials / Miscellaneous'] = "장신구 / 종특"
+
+L["Use /atemi command to open configuration UI."] = " /atemi 명령어로 설정창을 열수 있습니다."
+
+L['icon on nameplate'] = "사용"
+L['shows an icon with the cooldown timer atop the nameplate'] = "이름표 위에 재사용 타이머와 아이콘을 보여줍니다"
